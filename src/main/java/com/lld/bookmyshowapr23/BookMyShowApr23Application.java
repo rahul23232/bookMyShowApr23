d700c445b7cb5c2a14ffc08674dc9b9bac9dee19
rahul-pandey_395 <rahulpandey.dreams@gmail.com>
2024-04-13 13:51:30 +0530

    Initial commit

---
+++ src/main/java/com/lld/bookmyshowapr23/BookMyShowApr23Application.java
@@ -0,0 +1,13 @@
+package com.lld.bookmyshowapr23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowApr23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyShowApr23Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lld/bookmyshowapr23/BookMyShowApr23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lld.bookmyshowapr23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowApr23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
